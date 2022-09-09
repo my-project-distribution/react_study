276283dd7077a3a1f712739dc1b72247c892d271
kangbinkwon <kbk282655@gmail.com>
2022-09-09 23:41:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
+import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const name = 'fl';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>{name}리액트안녕</h1>
+    </>
   );
 }
 
