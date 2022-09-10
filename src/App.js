5f7239d32581604f3ec3bf8e5de4175074b7899a
kangbinkwon <kbk282655@gmail.com>
2022-09-11 01:54:37 +0900

    스타일

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 function App() {
-  const name = undefined;
-  const li = 'qkqhd';
-  return <div>{name || 'dLJ'}</div>
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : '44px',
+  };
+  const name = '리액트';
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
