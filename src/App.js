bf3ebab39dec643ae79071e898f39098720f6b2b
kangbinkwon <kbk282655@gmail.com>
2022-09-11 01:41:00 +0900

    undefined 렌더링

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 function App() {
-  const name = '리액트';
+  const name = undefined;
   const li = 'qkqhd';
-  return (
-    <div>
-      {
-        name==='리액트' && <h1>리액트</h1>
-      }
-    </div>
-  );
+  return <div>{name || 'dLJ'}</div>
 }
 
 export default App;
