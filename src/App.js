f6e1cccb019bd13c2947b2b45ecf4df3b28ddceb
kangbinkwon <kbk282655@gmail.com>
2022-09-10 11:16:24 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 function App() {
-  const name = 'fl';
+  const name = '리액트d';
   return (
-    <>
-    <h1>{name}리액트안녕</h1>
-    </>
+    <div>
+      {
+        name==='리액트' ? (
+          <h1>리액트입니다.</h1>
+        ) : (
+          <h2>리액트가 아닙니다.</h2>
+        )
+      }
+    </div>
   );
 }
 
