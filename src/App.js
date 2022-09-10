57fb1aca5e17802ffe594065491c3ecd7782a6d9
kangbinkwon <kbk282655@gmail.com>
2022-09-10 11:23:49 +0900

    조건문 렌더링

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 function App() {
-  const name = '리액트d';
+  const name = '리액트';
+  const li = 'qkqhd';
   return (
     <div>
       {
-        name==='리액트' ? (
+        name==='리액트'&&li==='qkqh' ? (
           <h1>리액트입니다.</h1>
-        ) : (
-          <h2>리액트가 아닙니다.</h2>
-        )
+        ) :( <h2>리액트가 아닙니다.</h2> )
       }
     </div>
   );
