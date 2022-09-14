287ff158db40b5fcdf89e903e945dd33c26d2307
kwonkangbin <kbk282655@gmail.com>
2022-09-14 19:51:44 +0900

    defaultProps

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,9 @@
-const MyComponent = () =>{
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = props =>{
+    return <div>나의 새롭고 멋진 {props.name}컴포넌트</div>;
+}
+
+MyComponent.defaultProps = {
+    name : '바보'
 }
 
 export default MyComponent;
