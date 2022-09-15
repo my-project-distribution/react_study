2ab5cd5d5c17d545f5c6e19f7423fee1625defc4
kwonkangbin <kbk282655@gmail.com>
2022-09-15 17:11:20 +0900

    PropTypes.number.isRequired

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,18 +2,21 @@ import { Children } from "react";
 import PropTypes from 'prop-types';
 
 const MyComponent = props =>{
-    const {name,children} = props;
+    const {name,favoriteNumber,children} = props;
     return <div>나의 새롭고 멋진 {name}컴포넌트 <br /> 
     Children값은 {children}입니다.
-    </div>;
+    <br />
+    내가 좋아하는 숫자는 {favoriteNumber}
+    </div>
 }
 
 MyComponent.defaultProps = {
-    name : '바보'
+    name : '바보',
 }
 
 MyComponent.propTypes = {
-    name : PropTypes.string
+    name : PropTypes.string,
+    favoriteNumber : PropTypes.number.isRequired
 }
 
 export default MyComponent;
