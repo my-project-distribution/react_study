64291779f8b6f20b14f89d392f24a697e88ff056
kwonkangbin <kbk282655@gmail.com>
2023-01-10 00:24:55 +0900

    인풋 관리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React,{ useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
-    const [number, setnumber] = useState(0);
-    return (
-    <div className="App">
-        <h1>{number}</h1>
-        <button onClick={()=>{setnumber(number+1)}}>+1</button>
-        <button onClick={()=>{setnumber(number-1)}}>-1</button>
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input onChange={onChange} value={text}/>
+      <button onClick={()=>{setText('');}}>초기화</button>
+      <h1>{text}</h1>
     </div>
   );
 }
